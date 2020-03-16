780285dc409c6d80ce9604435862435077583f65
Mariia_Palyga <mariia_palyga@epam.com>
2020-03-16 11:08:43 +0400

    commit 1)show events list

--- app/src/main/java/com/example/birthdayreminder/ui/Constants.java
+++ app/src/main/java/com/example/birthdayreminder/ui/Constants.java
@@ -5,6 +5,7 @@ public final class Constants {
     public final static String FIRST_NAME_KEY = "FIRST_NAME_KEY";
     public final static String BIRTHDAY_KEY = "BIRTHDAY_KEY";
     public final static String ID_KEY = "ID_KEY";
+    public static int ID_ALARM = 0;
 
     private Constants() {
     }
--- app/src/main/java/com/example/birthdayreminder/ui/settings/SetSettingsActivityView.java
+++
@@ -1,6 +0,0 @@
-package com.example.birthdayreminder.ui.settings;
-
-import com.example.birthdayreminder.base.BaseView;
-
-public interface SetSettingsActivityView extends BaseView {
-}
